79048c54a9b1182dbccbcfd5b4e351bf8b6e6d81
dgp97 <diana.gutierrez@tecsup.edu.pe>
2019-05-09 15:09:10 -0500

    lab

---
+++ Snapchat/iniciarSesionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by MAC17 on 9/05/19.
+//  Copyright © 2019 deah. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){(user, error) in
+            print("Intentando inicair sesion")
+            if(error != nil){
+                print("Se presenta el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
